d8feb437b17f21ea1851f7a39c0b7dc9c1259e6b
Tamas Molnar <125677315+Tomocza@users.noreply.github.com>
2024-01-31 11:54:23 +0100

    fix linker error caused by SDL2 redefining main()

--- Stratego/Stratego/main.cpp
+++ Stratego/Stratego/main.cpp
@@ -1,11 +1,17 @@
 // Stratego.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+//Needed for SDL2 to not redefine main() and therefore cause a linker error
+#define SDL_MAIN_HANDLED
+
 #include <iostream>
 #include <SDL2/SDL.h>
 
 int main()
 {
+	//Needed for SDL2 to not redefine main() and therefore cause a linker error
+	SDL_SetMainReady();
+
 	std::cout << "Hello World!\n";
 
 	return 0;
