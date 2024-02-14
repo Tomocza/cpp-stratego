8175fff8cf54bb769d24e189a41b1d7da7489898
Kristóf Németh <nemethkristof1@gmail.com>
2024-02-14 09:51:57 +0100

    Add player to piece, implement getters, make getAvailable moves return vector, and make const what can be const

--- Stratego/Stratego/Piece.cpp
+++ Stratego/Stratego/Piece.cpp
@@ -5,40 +5,57 @@
 #include "Spy.h"
 
 BattleOutcome Piece::attack(Piece* opponent) const
-{//attacker perspective
-	if(Bomb* bomb = dynamic_cast<Bomb*>(opponent)){
-		if (Miner* miner = dynamic_cast<Miner*>(const_cast<Piece*>(this))) {
-			return WIN; 
-		}
-		return LOSE;
-	}
-	if (Marshal* marshal = dynamic_cast<Marshal*>(opponent)) {
-		if (Spy* spy = dynamic_cast<Spy*>(const_cast<Piece*>(this))) {
+{
+	//attacker perspective
+	if (Bomb* bomb = dynamic_cast<Bomb*>(opponent))
+	{
+		if (Miner* miner = dynamic_cast<Miner*>(const_cast<Piece*>(this)))
+		{
 			return WIN;
 		}
 		return LOSE;
 	}
-	if (this->rank == opponent->rank) {
+	if (Marshal* marshal = dynamic_cast<Marshal*>(opponent))
+	{
+		if (Spy* spy = dynamic_cast<Spy*>(const_cast<Piece*>(this)))
+		{
+			return WIN;
+		}
+		return LOSE;
+	}
+	if (this->rank == opponent->rank)
+	{
 		return DRAW;
 	}
-	else {
+	else
+	{
 		return this->rank > opponent->rank ? WIN : LOSE;
 	}
 }
 
-std::set<Coordinate> Piece::getAvailableMoves() const
+std::vector<Coordinate> Piece::getAvailableMoves() const
 {
-	std::set<Coordinate> result;
-	for (int i = 0; i < movePattern.size(); i++) {
+	std::vector<Coordinate> result;
+	for (int i = 0; i < movePattern.size(); i++)
+	{
 		short x = position.x + movePattern.at(i).x;
 		short y = position.y + movePattern.at(i).y;
-		result.insert(Coordinate{x,y});
+		result.push_back(Coordinate{x, y});
 	}
 	return result;
 }
 
-Coordinate Piece::getPosition()
-{return position;}
+Coordinate Piece::getPosition() const
+{
+	return position;
+}
 
-void Piece::setPosition(Coordinate newPosition)
-{this->position = newPosition;}
+void Piece::setPosition(const Coordinate newPosition)
+{
+	this->position = newPosition;
+}
+
+Player Piece::getPlayer() const
+{
+	return player;
+}
--- Stratego/Stratego/Piece.h
+++ Stratego/Stratego/Piece.h
@@ -4,21 +4,26 @@
 #include "Coordinate.h"
 #include <set>
 
+#include "Player.h"
+
 class Piece
 {
 public:
-	explicit Piece(unsigned short rank)
-		: rank(rank)
+	explicit Piece(const unsigned short rank, const Player player)
+		: player(player), rank(rank)
 	{
 	}
 
 	BattleOutcome attack(Piece* opponent) const;
-	std::set<Coordinate> getAvailableMoves() const;
-	virtual ~Piece();
-	Coordinate getPosition();
-	void setPosition(Coordinate newPosition);
+	std::vector<Coordinate> getAvailableMoves() const;
+	virtual ~Piece() = default;
+	Coordinate getPosition() const;
+	void setPosition(const Coordinate newPosition);
+	Player getPlayer() const;
+
 private:
+	const Player player;
 	const unsigned short rank;
-	Coordinate position;
-	static const std::vector<DeltaCoordinate> movePattern;
+	Coordinate position{-1, -1};
+	std::vector<DeltaCoordinate> movePattern;
 };
